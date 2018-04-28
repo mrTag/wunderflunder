51d844301b21d8a2b6490af5369fcbc167d6f5a2
Patrick Wachowiak <patrick@chasing-carrots.com>
2018-04-28 14:07:56 +0200

    car collider

--- Assets/scripts/Car.cs
+++ Assets/scripts/Car.cs
@@ -4,12 +4,37 @@ using UnityEngine;
 
 public class Car : MonoBehaviour {
 
+	public enum DriverType {
+		Babsi,
+		Bob
+	}
+	
+	public DriverType Driver;
+	public Transform Spawnpoint;
+	public GameObject BabsiPrefab;
+	public GameObject BobPrefab;
 	public float MotorTorque = 50;
+	public GameObject ChassisBob;
+	public GameObject ChassisBabsi;
 
 	private WheelCollider[] _wheelColliders;
 
 	void Start () {
 		_wheelColliders = GetComponentsInChildren<WheelCollider>();
+		switch (Driver) {
+			case DriverType.Babsi:
+				GameObject Babsi = Instantiate(BabsiPrefab, Spawnpoint,false);
+				
+				ChassisBob.SetActive(false);
+				ChassisBabsi.SetActive(true);
+			break;
+			case DriverType.Bob:				
+				GameObject Bob = Instantiate(BobPrefab, Spawnpoint,false);
+
+				ChassisBob.SetActive(true);
+				ChassisBabsi.SetActive(false);
+			break;
+		}
 	}
 	
 	void FixedUpdate () {
